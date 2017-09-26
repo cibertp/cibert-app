dcc0d06b7241b90120bc3f40656390dceed7f791
cibertp <cibertp462@gmail.com>
2017-09-26 18:03:15 -0500

    incluyento el bottombar  recursos utilizados

---
+++ app/src/main/java/com/cibertpoet/cibert_app/view/ConteinerActivity.java
@@ -0,0 +1,15 @@
+package com.cibertpoet.cibert_app.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.cibertpoet.cibert_app.R;
+
+public class ConteinerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_conteiner);
+    }
+}
